fc8a13e65bad21f577901f9da447d01f4465a736
Soporte Simil <soporte@similtech.co>
2020-04-07 18:38:10 -0500

    Added Memento Pattern

---
+++ Memento/src/memento/MementoDemo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memento;
+
+/**
+ *
+ * @author DarkFACS
+ */
+public class MementoDemo {
+
+    public static void main(String[] args) {
+        Caretaker caretaker = new Caretaker();
+        Originator originator = new Originator();
+        originator.setState("State1");
+        originator.setState("State2");
+        caretaker.addMemento( originator.save() );
+        originator.setState("State3");
+        caretaker.addMemento( originator.save() );
+        originator.setState("State4");
+        originator.restore( caretaker.getMemento() );
+    }
+}
